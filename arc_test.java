46816dde2698f17afd1cd70fc2050e8a5f9f43b2
WilliamTheBeginner <william78smart@hotmail.com>
2019-05-30 09:13:21 -0400

    New commit, con

---
+++ arc_test.java
@@ -0,0 +1,33 @@
+import arc.*;
+
+public class arc_test
+{
+  public static void main(String[] args)
+  {
+    Console con = new Console(1920, 1080);
+
+    int ovalX = (int)(Math.random() * 1770);
+    int ovalY = (int)(Math.random() * 930);
+
+    con.drawOval(ovalX, ovalY, 150, 150);
+
+    int ovalRadX = ovalX + 75;
+    int ovalRadY = ovalY + 75;
+
+    con.drawLine(ovalRadX, ovalRadY, 1200, 700);
+
+    int foo = 1;
+
+    while(true)
+    {
+      foo++;
+      if(con.currentMouseButton() == 1)
+      {
+        if(((Math.pow((con.currentMouseX() - ovalRadX), 2) + Math.pow((con.currentMouseY() - ovalRadY), 2)) < Math.pow(75, 2)))
+        {
+          con.println(foo);
+        }
+      }
+    }
+  }
+}
--- draw_test.java
+++ draw_test.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
-import java.swing.*;
+import javax.swing.*;
 
 public class draw_test extends JFrame{
   public static void main(String[] args)
@@ -11,31 +11,45 @@ public class draw_test extends JFrame{
     final int FRAME_WIDTH = 300;
     final int FRAME_HEIGHT = 400;
 
-    frame.setSize(FRAME_WIDTH, FRAME_HEIGHT):
+    frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 
     frame.setTitle("Two Rectangles");
 
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    Rectangle rect = new Rectangle();
+    DrawRectangle rect = new DrawRectangle();
 
     frame.add(rect);
+
+    frame.pack();
+
+    @Override
+    frame.addComponentListener(new ComponentListener()
+    {
+      public void ComponentMoved(ComponentEvent e)
+      {
+        Component c = (Component) e.getSource();
+
+        Point newLoc = c.getLocation();
+        System.out.println("Get new location");
+      }
+    });
 
     frame.setVisible(true);
   }
 }
 
-class Rectangle extends JComponent
+class DrawRectangle extends JComponent
 {
   public void paintComponent(Graphics g)
   {
-    Graphics2D g2 = (Grapahics2D) g;
+    Graphics2D g2 = (Graphics2D) g;
 
     Rectangle box = new Rectangle(5, 10, 20, 30);
 
     g2.draw(box);
 
-    box.translate(15, 25);
+    box.translate((int)(Math.random() * 300), (int)(Math.random() * 400));
 
     g2.draw(box);
 
