d608d6f7d0b0ad539582fe13db8e9f6e89ff016b
WilliamTheBeginner <william78smart@hotmail.com>
2019-05-30 14:20:07 -0400

    new commit

--- arc_test.java
+++ arc_test.java
@@ -1,17 +1,18 @@
 import arc.*;
 import java.util.Arrays;
 
-public class cpt_test
+public class arc_test
 {
   public static void main(String[] args)
   {
     Console con = new Console(1920, 1080);
-    
+
     Circle cir = new Circle();
-    
+
     cir.draw(con);
-    
+
     con.println(Arrays.toString(cir.getRadXY()));
+
 
 	/*
     int ovalX = (int)(Math.random() * 1770);
@@ -19,13 +20,13 @@ public class cpt_test
 
     con.drawOval(ovalX, ovalY, 150, 150);
 
-	
+
     int ovalRadX = ovalX + 75;
     int ovalRadY = ovalY + 75;
 
     con.drawLine(ovalRadX, ovalRadY, 1200, 700);
     */
-    
+
     clickCircle(cir, con);
 
     int foo = 1;
@@ -33,23 +34,28 @@ public class cpt_test
   }
   public static boolean clickCircle(Circle circle, Console con)
   {
-	  Console con2 = new Console();
-	  
+	  //Console con2 = new Console();
+
 	  int [] radarr = new int[2];
-	  
+
 	  radarr = circle.getRadXY();
-	  
+
 	  int radX = radarr[0];
-	  int radY = radarr[1]; 
-	  
-	  int foo = 0; 
-	  
+	  int radY = radarr[1];
+
+	  int foo = 0;
+
+    con.println(radX);
+    con.println(radY);
+
+    con.println(circle.getRadius());
+
 	  while(true)
 	  {
 		  foo++;
 		  if(con.currentMouseButton() == 1)
 		  {
-			  if(((Math.pow((con.currentMouseX() - radX), 2) + Math.pow((con.currentMouseY() - radY), 2)) < Math.pow(75, 2)))
+			  if(((Math.pow((con.currentMouseX() - radX), 2) + Math.pow((con.currentMouseY() - radY), 2)) < Math.pow(circle.getRadius(), 2)))
 			  {
 				  con.println(foo);
 			  }
@@ -61,7 +67,7 @@ class Circle
 {
   final int dimX = 1920;
   final int dimY = 1080;
-  
+
   private int radius;
   private int radX;
   private int radY;
@@ -69,36 +75,37 @@ class Circle
   public Circle()
   {
 	  this.radius = 75;
-	  this.radX = (int)(Math.random() * 1770);
-	  this.radY = (int)(Math.random() * 930);
+	  this.radX = (int)(Math.random() * 1845);
+	  this.radY = (int)(Math.random() * 1005);
   }
-  
+
+  // experiementory. The radiuses cannot exceed the dimensions
   public Circle(int radius, int radX, int radY)
   {
 	  this.radius = radius;
 	  this.radX = radX;
 	  this.radY = radY;
   }
-  
+
   public int getRadius()
   {
 	  return radius;
   }
-  
+
   public int [] getRadXY()
   {
 	  int arr[] = new int[2];
-	  
+
 	  arr[0] = radX;
 	  arr[1] = radY;
-	  
+
 	  return arr;
   }
-  
+
   public void draw(Console con)
   {
-	  int drawX = dimX - radX;
-	  int drawY = dimY - radY;
+	  int drawX = radX - radius;
+	  int drawY = radY - radius;
 	  con.drawOval(drawX, drawY, radius * 2, radius * 2);
   }
 }
