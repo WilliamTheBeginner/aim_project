c10b28b8a20a4cc8e9a743e5050cd6465cbebc36
WilliamTheBeginner <william78smart@hotmail.com>
2019-06-14 19:31:37 -0400

    Update Button.java

--- src/Button.java
+++ src/Button.java
@@ -5,11 +5,13 @@ import src.*;
 
 public class Button
 {
+  // initialize button properties
   private int intLength;
   private int intWidth;
   private int intDimX;
   private int intDimY;
 
+  // button class constructor
   public Button(int intDimX, int intDimY, int intLength, int intWidth)
   {
     this.intLength = intLength;
@@ -18,10 +20,12 @@ public class Button
     this.intDimY = intDimY;
   }
 
+  // if this method is called, draw a rectangle of the button dimensions onto the console
   public void draw(Console con)
   {
     con.drawRect(intDimX, intDimY, intLength, intWidth);
   }
+  // if this method is called, get the dimensions of the button, store into array and return it
   public int [] getDims()
   {
     int intDim[] = new int[2];
